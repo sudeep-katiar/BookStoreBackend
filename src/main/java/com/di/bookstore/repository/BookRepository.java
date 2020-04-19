663dd92e5d9a7d109bb5627baebfa44e14f26c96
sudeep-katiar <chandanpro32@gmail.com>
2020-04-19 19:51:42 +0530

    [sudeep-katiar].modified book repository

--- src/main/java/com/di/bookstore/repository/BookRepository.java
+++ src/main/java/com/di/bookstore/repository/BookRepository.java
@@ -14,19 +14,30 @@ import com.di.bookstore.model.BookModel;
 @Repository
 @Transactional
 public interface BookRepository extends JpaRepository<BookModel, Long> {
-	
+
 	@Query(value = "select * from book_detail where book_id = :id", nativeQuery = true)
 	BookModel findById(long id);
 
 	@Modifying
 	@Query(value = "insert into book_detail (book_name, book_code, book_details, price, quantity, author_name) values (:bookName, :bookCode, :bookDetails, :price, :quantity, :authorName)", nativeQuery = true)
-	void insertData(String bookName, String bookCode, String bookDetails, double price, int quantity, String authorName);
+	void insertData(String bookName, String bookCode, String bookDetails, double price, int quantity,
+			String authorName);
 
 	@Modifying
-	@Query(value = "update book_detail set add_to_bag = :b where user_id = :userid AND id = :id" ,  nativeQuery = true)
+	@Query(value = "update book_detail set add_to_bag = :b where user_id = :userid AND id = :id", nativeQuery = true)
 	void bag(boolean b, long userid, long id);
 
 	@Query(value = "select * from book_detail where user_id = :userId", nativeQuery = true)
-	List<BookModel> getAllForUser(long userId);
+	List<BookModel> getAllForAdmin(long userId);
+
+	@Query(value = "select * from book_detail where user_id = :userId and add_to_bag = true", nativeQuery = true)
+	List<BookModel> getCartBooks(long userId);
+
+	@Modifying
+	@Query(value = "update book_detail set wishlist = :b where user_id = :userid AND id = :id", nativeQuery = true)
+	void setWishlist(boolean b, long userid, long id);
+
+	@Query(value = "select * from book_detail where user_id = :userId and wishlist = true", nativeQuery = true)
+	List<BookModel> getWishlistBooks(long userId);
 
 }
