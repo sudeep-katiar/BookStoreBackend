57d42602ffb7807413f7184dddd089d8d9f7ee46
sudeep-katiar <chandanpro32@gmail.com>
2020-04-19 19:50:39 +0530

    [sudeep-katiar].modified user controller

--- src/main/java/com/di/bookstore/controller/UserController.java
+++ src/main/java/com/di/bookstore/controller/UserController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,6 +30,7 @@ import io.swagger.annotations.ApiOperation;
  */
 
 @RestController
+@CrossOrigin(allowedHeaders = "*", origins = "*", exposedHeaders = { "jwtToken" })
 @RequestMapping("/user")
 public class UserController {
 	
