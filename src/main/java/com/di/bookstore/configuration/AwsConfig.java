a6c353f94b0fadd322a4c9ba062edd7a94ee08a4
sudeep-katiar <chandanpro32@gmail.com>
2020-04-20 12:11:02 +0530

    [sudeep-katiar].added aws configuration

---
+++ src/main/java/com/di/bookstore/configuration/AwsConfig.java
@@ -0,0 +1,30 @@
+package com.di.bookstore.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+public class AwsConfig {
+	
+	@Value("${aws.keyId}")
+    private String awsKeyId;
+
+    @Value("${aws.accessKey}")
+    private String accessKey;
+
+    @Value("${aws.region}")
+    private String region;
+
+    @Bean
+    public AmazonS3 awsS3Client() {
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials(awsKeyId, accessKey);
+        return AmazonS3ClientBuilder.standard().withRegion(Regions.fromName(region))
+                .withCredentials(new AWSStaticCredentialsProvider(awsCreds)).build();
+    }
+
+}
