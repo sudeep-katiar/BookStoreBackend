a9b1f631e77eebb2812b5641c5b77ab286eda531
sudeep-katiar <chandanpro32@gmail.com>
2020-04-22 17:29:46 +0530

    [sudeep-katiar].modified Book ServiceImpl

--- src/main/java/com/di/bookstore/serviceimpl/BookServiceImpl.java
+++ src/main/java/com/di/bookstore/serviceimpl/BookServiceImpl.java
@@ -1,10 +1,16 @@
 package com.di.bookstore.serviceimpl;
 
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.di.bookstore.dto.BookDto;
 import com.di.bookstore.model.BookModel;
@@ -14,19 +20,19 @@ import com.di.bookstore.repository.UserRepository;
 import com.di.bookstore.service.BookService;
 import com.di.bookstore.util.Jwt;
 
-/**
+/**Pat
  * This class implements {@link BookService} interface which has the
  * unimplemented functionality of adding a book, updating, deleting and
  * getallbooks. All operations will be carried out if the user is a valid user.
  * 
- * @author Sudeep Kumar Katiar
+ * @author Sudeep Kumar Katiar/home/akash/Pictures
  * @created 13/4/2020
  * @version 1.0
  */
 @Service
 public class BookServiceImpl implements BookService {
 	
-//	private byte[] bytes;
+	private String location="assets/logo.jpg";
 
 	@Autowired
 	private Jwt tokenGenerator;
@@ -51,21 +57,20 @@ public class BookServiceImpl implements BookService {
 		if (user != null) {
 			BookModel book = new BookModel(bookdto.getBookName(), bookdto.getBookCode(), bookdto.getBookDetails(),
 					bookdto.getPrice(), bookdto.getQuantity(), bookdto.getAuthorName());
-//			book.setPicByte(this.bytes);
 			book.setAddToBag(false);
 			book.setWishlist(false);
+			book.setFilepath(location);
 			book.setCreatedBy(user);
 //			bookRepository.insertData(book.getBookName(), book.getBookCode(), book.getBookDetails(), book.getPrice(),
-//					book.getQuantity(), book.getAuthorName(), book.getCreatedBy().getId());
+//					book.getQuantity(), book.getAuthorName(), book.getCreatedBy(), book.isAddToBag(), book.isWishlist());
 			bookRepository.save(book);
-//			this.bytes = null;
 			return book;
 		}
 		return null;
 	}
 
 	@Override
-	public int updateCart(BookDto notedto, String token, long id) {
+	public int updateCart(BookDto notedto, String token, int id) {
 		long userid = tokenGenerator.parseJwtToken(token);
 		Optional<UserModel> user = userRepository.findById(userid);
 		if (user != null) {
@@ -115,7 +120,7 @@ public class BookServiceImpl implements BookService {
 	}
 
 	@Override
-	public int updateWishlist(BookDto notedto, String token, long id) {
+	public int updateWishlist(BookDto notedto, String token, int id) {
 		long userid = tokenGenerator.parseJwtToken(token);
 		Optional<UserModel> user = userRepository.findById(userid);
 		if (user != null) {
@@ -145,15 +150,18 @@ public class BookServiceImpl implements BookService {
 		return null;
 	}
 
-//	@Override
-//	public int upload(MultipartFile file, String token) throws IOException {
-//		long userId = tokenGenerator.parseJwtToken(token);
-//		Object isUserAvailable = userRepository.findById(userId);
-//		if (isUserAvailable != null) {
-//			this.bytes = file.getBytes();
-//			return 1;
-//		}
-//		return 0;
-//	}
+	@Override
+	public int upload(MultipartFile file, String token, int id) throws IOException {
+		long userId = tokenGenerator.parseJwtToken(token);
+		Object isUserAvailable = userRepository.findById(userId);
+		if (isUserAvailable != null) {
+			byte[] bytes = file.getBytes();
+			Path path = Paths.get("/home/akash/Downloads/SudeepFrontend/src/assets/photos/" + file.getOriginalFilename());
+			Files.write(path, bytes);
+			bookRepository.uploadImage(id, "assets/photos/"+file.getOriginalFilename());
+			return 1;
+		}
+		return 0;
+	}
 
 }
