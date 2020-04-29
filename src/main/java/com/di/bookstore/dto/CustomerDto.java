25e708c81f6d3c833ed76d978b4c7552bbbbf192
sudeep-katiar <chandanpro32@gmail.com>
2020-04-30 00:27:13 +0530

    [sudeep-katiar].CustomerDto

---
+++ src/main/java/com/di/bookstore/dto/CustomerDto.java
@@ -0,0 +1,78 @@
+package com.di.bookstore.dto;
+
+public class CustomerDto {
+
+	private String name;
+	private String email;
+	private long phone;
+	private long pincode;
+	private String locality;
+	private String address;
+	private String city;
+	private String landmark;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public long getPhone() {
+		return phone;
+	}
+
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+
+	public long getPincode() {
+		return pincode;
+	}
+
+	public void setPincode(long pincode) {
+		this.pincode = pincode;
+	}
+
+	public String getLocality() {
+		return locality;
+	}
+
+	public void setLocality(String locality) {
+		this.locality = locality;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getLandmark() {
+		return landmark;
+	}
+
+	public void setLandmark(String landmark) {
+		this.landmark = landmark;
+	}
+
+}
